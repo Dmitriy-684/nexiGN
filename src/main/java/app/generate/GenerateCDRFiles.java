fdd4081ccb1ffa9fdccd08821e5b62e130d2966a
David <david.stepanyan2014@mail.ru>
2024-03-22 20:16:22 +0300

    Added generatorTime method

--- src/main/java/app/generate/GenerateCDRFiles.java
+++ src/main/java/app/generate/GenerateCDRFiles.java
@@ -3,12 +3,23 @@ package app.generate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.*;
+
 import java.util.*;
 
 @Service
 public class GenerateCDRFiles {
-    @Value("${telephones.count}")
+    @Value("${generateCDRFiles.telephones.count}")
     public int countOfPhoneNumbers;
+
+    @Value("${generateCDRFiles.currentYear}")
+    public int year;
+    @Value("${generateCDRFiles.callMinTime}")
+    public int differentMin;
+    @Value("${generateCDRFiles.callMaxTime}")
+    public int differentMax;
+
+
     public Set<String> generateTelephoneNumbers() {
         Random random = new Random();
         Set<String> listOfNumbers = new HashSet<>();
@@ -19,5 +30,25 @@ public class GenerateCDRFiles {
             listOfNumbers.add(builder.toString());
         }
         return listOfNumbers;
+    }
+
+public List<Long> generatorTime(Month month){
+        List<Long> listTime = new ArrayList<>();
+        Random random = new Random();
+
+    int daysInMonth = month.length(Year.isLeap(year));
+
+    Instant dateStart = LocalDate.of(year, month, 1).atStartOfDay().atZone(ZoneId.of("UTC")).toInstant();
+    Instant dateEnd = LocalDate.of(year, month, daysInMonth).atStartOfDay().atZone(ZoneId.of("UTC")).toInstant();
+
+        int different = random.nextInt(differentMin, differentMax);
+
+        for(long i = dateStart.getEpochSecond(); i < dateEnd.getEpochSecond(); i += different){
+            listTime.add(i);
+        }
+        if (listTime.size() % 2 != 0){
+            listTime.remove(listTime.size() - 1);
+        }
+        return listTime.stream().sorted().toList();
     }
 }
--- src/test/java/JavaTest.java
+++ src/test/java/JavaTest.java
@@ -3,14 +3,27 @@ import app.generate.GenerateCDRFiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.time.Month;
+import java.time.Year;
+
 public class JavaTest {
     @Test
     public void test() {
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+//
+//        GenerateCDRFiles generator = context.getBean("generateCDRFiles", GenerateCDRFiles.class);
+//        System.out.println(generator.generateTelephoneNumbers());
+        int daysInMonth = Month.FEBRUARY.length(Year.isLeap(2023));
+        System.out.println(daysInMonth);
+//        context.close();
+
+    }
+    @Test
+    public void test1(){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
         GenerateCDRFiles generator = context.getBean("generateCDRFiles", GenerateCDRFiles.class);
-        System.out.println(generator.generateTelephoneNumbers());
-
+        System.out.println(generator.generatorTime(Month.DECEMBER));
         context.close();
     }
 }
