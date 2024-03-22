98d263ddbb526d4a9a77e87b4ac9710e41a08c91
dmitr <dmitriyegorov111@mail.ru>
2024-03-22 21:49:22 +0300

    create H2 database

--- src/main/java/app/utils/InitiateUtils.java
+++ src/main/java/app/utils/InitiateUtils.java
@@ -2,9 +2,11 @@ package app.utils;
 
 import app.entities.SubscriberEntity;
 import app.entities.TransactionEntity;
+import app.generate.GenerateCDRFiles;
 import app.services.SubscribersService;
 import app.services.TransactionsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +15,16 @@ import org.springframework.stereotype.Service;
 public class InitiateUtils implements CommandLineRunner {
     private final SubscribersService subscribersService;
     private final TransactionsService transactionsService;
+    @Autowired
+    private final GenerateCDRFiles generator;
     @Override
     public void run(String... args) throws Exception {
-        SubscriberEntity subscriberEntity = new SubscriberEntity();
-        subscriberEntity.setPhone("89303569144");
-        TransactionEntity transactionEntity = new TransactionEntity();
-        transactionEntity.setCallType("01");
-        transactionEntity.setStartCall("174");
-        transactionEntity.setEndCall("175");
-        subscriberEntity.addTransaction(transactionEntity);
-        subscribersService.save(subscriberEntity);
-        transactionsService.save(transactionEntity);
-        System.out.println(subscriberEntity);
+        for (String phone : generator.generateTelephoneNumbers()) {
+            SubscriberEntity subscriberEntity = new SubscriberEntity();
+            TransactionEntity transactionEntity = new TransactionEntity();
+            subscriberEntity.setPhone(phone);
+            subscribersService.save(subscriberEntity);
+        }
+        for (SubscriberEntity subscriberEntity : subscribersService.getAll()) System.out.println(subscriberEntity.toString());
     }
 }
