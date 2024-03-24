d058d48156b99b48d21687fe222207219757240b
dmitr <dmitriyegorov111@mail.ru>
2024-03-25 00:03:39 +0300

    add maven plugins

--- src/test/java/app/udrServices/UDRFileManagerTest.java
+++ src/test/java/app/udrServices/UDRFileManagerTest.java
@@ -1,8 +1,6 @@
 package app.udrServices;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
 import java.io.IOException;
